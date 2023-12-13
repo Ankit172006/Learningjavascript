006c2fe042f66229e637eeff6ad43976e30c3883
Karan <karantha289@gmail.com>
2023-12-13 16:34:40 +0530

    Learning java Script

---
+++ ankit.js
@@ -0,0 +1,278 @@
+
+//<---- calculate convert hour into seconds---->//
+
+// const num=prompt("Enter a hours convert into seconds")
+// function array(hour)
+// {
+//   return hour*60*60;
+// }
+// console.log(array(num));
+
+//<----Return the first element if array---->//
+
+// function array(arr)
+// {
+//     arr=[15,85,64];
+//     return arr[0];
+
+// }
+// console.log(array())
+
+//<----Power calculater---->//
+
+// const x=prompt("The value of x to the power of y (xy) 'enter x'  ");
+// const y=prompt("x to the power of y (xy) 'Enter y");
+// function calpower(a,b)
+// {
+//     var c=Math.pow(a,b);
+//     return c;
+// }
+
+// console.log(calpower(x,y));
+
+
+//<----Return the remainder of two number---->
+
+
+// const num1=prompt("Enter the first number");
+// const num2=prompt("Enter the second number");
+// function remainder(x,y)
+// {
+//     var remainder=x%y;
+//     return remainder;
+// }
+// console.log("Remainder")
+// console.log(remainder(num1,num2))
+
+
+
+
+// function calrectangle(x,y)
+// {
+//     return 2*(x,y);
+// }
+// console.log(25,86)
+
+//<----- lessthen 100----->
+
+
+// function raman(x,y)
+// {
+//     if(x>99 && y>99)
+//     {  return console.log("True");
+       
+//     }
+//     else
+//     {
+//         return console.log("False");
+        
+//     }
+// }
+// console.log(raman)1,2));
+
+// function string(x,y)
+// {
+//     var a="Something";
+//     var b=" ";
+//     return a+b+"is better then nothing"
+// }
+// console.log(string())
+
+
+// function lessThan(num) {
+// 	if(num<=0)
+// 		{
+// 			return "true";
+// 		}
+// 	else
+// 		{
+// 			return "false";
+// 		}
+// }
+// console.log(lessThan(5));
+
+//<----- change aray value using ananymsfunction/callbackfunction ------->//
+
+// var a=[1,2,3,4,5];
+
+//  var z=a.map((x)=>{
+// 	return x*2;
+//   })
+
+//  console.log(z);
+
+// function hdmi(x)
+// {
+// 	return(x-2)*180;
+// }
+// console.log(10);
+
+//<---Basketball---->//
+
+// function basket(x,y)
+// {
+// 	var t=(x*2)+(y*3);
+// 	return t;
+// }
+// console.log(basket(5,20));
+
+//<----Re number equal--->
+// function equal(num1,num2)
+// {
+//     if(num1==num2)
+//     {
+//         return "true";
+//     }
+//     else
+//     {
+//         return "false";
+//     }
+// }
+// console.log(equal(5,8))
+
+// var ankit=[{name:"Ankit",address:"Mandi,Himachal Perdash"},{name:"Pankaj",address:"Chandigarh"}];
+// var z=ankit.map((d)=>
+// {
+//   return d.name+" "+d.address;
+// }
+// )
+// console.log(z);
+
+
+// var let and const varable
+// {
+//  var a=10;
+// let b=20;
+
+// }
+// const c=30;
+//     console.log(c)
+
+//     var a=12;
+//     let b=10;
+//     const c=50;
+//     e=a+b+c;
+//   console.log("Total amt is",e);
+
+//  var num1=1000;
+//  var num2=269;
+//  var num3=586;
+// {
+//  console.log(num1+num2/num3);    
+//  console.log(num1/num2*num3);
+//  console.log(num1*num2-num3);
+//  console.log(num1-num2+num3);    
+// }
+// {
+//       var a=20;
+//       let b=50;
+//       const c=10; 
+
+//       console.log(b)
+//       console.log(c)
+// }
+// console.log(a)
+
+
+
+
+//<||-------Operators-------||>//
+       // Arithmetic Operators
+       // Assignment Operators
+       // Comparison Operators
+       // String Operators
+       // Logical Operators
+       // Bitwise Operators
+       // Ternary Operators
+       // Type Operators
+
+
+      // ---- Arithmetic Operators ---- //
+
+      // +	       Addition
+      // -	       Subtraction
+      // *	       Multiplication
+      // **	       Exponentiation (ES2016)
+      // /	       Division
+      // %	       Modulus (Division Remainder)
+      // ++	       Increment
+      // --	       Decrement
+
+//  let a=30;
+//  let b="85";
+ 
+//  console.log(a+b);
+//  console.log(a-b);
+//  console.log(a/b);
+//  console.log(a*b);
+//  console.log(a%b);
+
+//  console.log(a==b);
+//  console.log(a===b);
+
+//  string operators
+
+
+//Enter your marks
+
+let marks =367;
+
+if(marks>=400 && marks<=500)
+{
+      console.log(`You score a ${marks/5} % marks in +2 Examination and You got A grade `)
+}
+else if(marks>=300 && marks<=500)
+{
+      console.log(`You score a ${marks/5} % marks in +2 Examination and You got B grade `)
+}
+else if(marks>=250 && marks<=500)
+{
+      console.log(`You score a ${marks/5} % marks in +2 Examination and You got C grade `)
+}
+else if(marks>=180 && marks<=500)
+{
+      console.log(`You score a ${marks/5} % marks in +2 Examination and You got D grade `)
+  
+}
+else if(marks>500)
+{
+      console.log("Please Enter Valid number")
+}
+else
+{
+      console.log(`You score a ${marks/5} % marks.Sorry Dear,You are Fail in +2 Examination  `)
+}
+
+// const age=111;
+
+// if(age>=18 && age<=60)
+// {
+//       console.log("You can drive");
+// }
+// else if(age<18 && age>14)
+// {
+// console.log("You can't Drive");
+// }
+// else if(age<14)
+// {
+//       console.log("Abhi aap Chote Bache hai Kripya gadi ma hi bathe rhe")
+// }
+// else if(age>60 && age<100)
+// {
+//    console.log("Kripya gadi chalane ki echha na rkhe apni family ha sath injoy kre")
+// }
+// else if(age>=100 && age<=110)
+// {
+//       console.log("Aap 100's kar chuke ha aap rhne hi de aap ki baat hi alg ha")
+// }
+// else
+// {
+//       console.log("Aap upre se dhyan rhe")
+// }
+
+//  console.log("Counting with loop");
+// for(let i=1;i<=1000;i++)
+// {     
+//       console.log(i)
+// }
+
